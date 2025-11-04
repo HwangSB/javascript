927c52be0e158028780a860125d6027ff8b84c0a
Vaggelis Yfantis <me@octoper.me>
2025-11-04 13:54:44 +0200

    feat(clerk-js): Implement reset password task

--- packages/shared/src/types/appearance.ts
+++ packages/shared/src/types/appearance.ts
@@ -1038,6 +1038,7 @@ export type SubscriptionDetailsTheme = Theme;
 export type APIKeysTheme = Theme;
 export type OAuthConsentTheme = Theme;
 export type TaskChooseOrganizationTheme = Theme;
+export type TaskResetPasswordTheme = Theme;
 
 type GlobalAppearanceOptions = {
   /**
