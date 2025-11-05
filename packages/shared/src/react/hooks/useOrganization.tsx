3a320a65bfddcc298dd31539daae09e636d94b8c
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2025-11-05 11:42:40 -0300

    Add HOC to open modal based on org settings

--- packages/shared/src/react/hooks/useOrganization.tsx
+++ packages/shared/src/react/hooks/useOrganization.tsx
@@ -1,4 +1,4 @@
-import { getCurrentOrganizationMembership } from '../../organization';
+import { getCurrentOrganizationMembership, withOrganizationSettingsEnabled } from '../../organization';
 import { eventMethodCalled } from '../../telemetry/events/method-called';
 import type {
   ClerkPaginatedResponse,
@@ -19,6 +19,7 @@ import {
   useSessionContext,
 } from '../contexts';
 import type { PaginatedHookConfig, PaginatedResources, PaginatedResourcesWithDefault } from '../types';
+import { useClerk } from './useClerk';
 import { usePagesOrInfinite, useWithSafeValues } from './usePagesOrInfinite';
 
 /**
@@ -270,7 +271,7 @@ const undefinedPaginatedResource = {
  * }
  * ```
  */
-export function useOrganization<T extends UseOrganizationParams>(params?: T): UseOrganizationReturn<T> {
+function useOrganizationInternal<T extends UseOrganizationParams>(params?: T): UseOrganizationReturn<T> {
   const {
     domains: domainListParams,
     membershipRequests: membershipRequestsListParams,
@@ -470,3 +471,5 @@ export function useOrganization<T extends UseOrganizationParams>(params?: T): Us
     invitations,
   };
 }
+
+export const useOrganization = withOrganizationSettingsEnabled(useOrganizationInternal, () => useClerk());
